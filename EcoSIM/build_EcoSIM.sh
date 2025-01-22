55f231bd0622d59d9f7a56ca2f302f4d444f65f7
leizhang-geo <lei.zhang.geo@outlook.com>
2025-01-21 17:35:52 -0800

    update

--- EcoSIM/build_EcoSIM.sh
+++ EcoSIM/build_EcoSIM.sh
@@ -32,12 +32,6 @@ precision="double"
 prefix=""
 systype=""
 
-### LZ code edit START ###
-systype="Linux"
-precision="double"
-shared=1
-### LZ code edit END   ###
-
 print_help() {
     echo "Usage: $0 [OPTIONS]"
     echo "Options:"
